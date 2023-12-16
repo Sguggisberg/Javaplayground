ff442b9e962b34211c40944de778222d35c28f29
Stefan Guggisberg <stefanguggisberg76@gmail.com>
2023-12-16 10:53:58 +0100

    Comparator with lambda

--- arrowplay/src/arrowplay/functions/FunctionTest.java
+++
@@ -1,9 +0,0 @@
-package arrowplay.functions;
-
-public class FunctionTest {
-
-	public static void main(String[] args) {
-		System.out.println("Test");
-	}
-
-}
---
+++ arrowplay/src/ch/stg/lambdas/ComparatorLamdaExample.java
@@ -0,0 +1,30 @@
+package ch.stg.lambdas;
+
+import java.util.Comparator;
+
+public class ComparatorLamdaExample {
+	public static void main(String[] args) {
+		Comparator<Integer> comparator = new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o1.compareTo(o2);
+			}
+		};
+		System.out.println("Vergleiche: " + comparator.compare(3, 2));
+		
+		Comparator<Integer> comparatorLamda = (Integer a, Integer b)-> {
+			return a.compareTo(b);
+		};
+		
+		System.out.println("Vergleiche: " + comparatorLamda.compare(3, 2));
+
+		
+		Comparator<Integer> comparatorLamda2 = ( a,  b)-> {
+			return a.compareTo(b);
+		};
+		
+		System.out.println("Vergleiche: " + comparatorLamda2.compare(3, 2));
+		
+	}
+}
