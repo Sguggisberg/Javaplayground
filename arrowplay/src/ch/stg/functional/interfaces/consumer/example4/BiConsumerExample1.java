f76a53d53a5c4026a7d5cf1fcee14f8ebe865e8d
Stefan Guggisberg <stefanguggisberg76@gmail.com>
2023-12-26 09:23:55 +0100

    Predicate Exampe2

---
+++ arrowplay/src/ch/stg/functional/interfaces/consumer/example4/BiConsumerExample1.java
@@ -0,0 +1,10 @@
+package ch.stg.functional.interfaces.consumer.example4;
+
+public class BiConsumerExample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
