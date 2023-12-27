c0d222f2c4c252440a3b16d4388e49b978049b9f
u240749 <stefan.guggisberg2@sbb.ch>
2023-12-27 08:59:08 +0100

    Functional Interfaces with 2 examples.

--- arrowplay/src/ch/stg/functional/interfaces/example1/Example1.java
+++ arrowplay/src/ch/stg/functional/interfaces/example1/Example1.java
@@ -3,10 +3,13 @@ package ch.stg.functional.interfaces.example1;
 import java.util.function.Function;
 
 public class Example1 {
-    static Function<String, String> function = (name) -> name.toUpperCase();
+    private static Function<String, String> function = (name) -> name.toUpperCase();
+    private static Function<String, String> addSomeString = (name) -> name.toUpperCase().concat("default");
 
     public static void main(String[] args) {
-        System.out.println("Text: " + function.apply("Java8"));
+        System.out.println("Result: " + function.apply("Java8"));
+        System.out.println("Result of andthen is: " + function.andThen(addSomeString).apply("Java8"));
+        System.out.println("Result of andthen is: " + function.compose(addSomeString).apply("Java8"));
     }
 
 }
